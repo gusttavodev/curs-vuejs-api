2acd5cfe05bf95502a7cbb2baa7766a984102b4c
Gustavo Araujo <gustavo.araujo@devsquad.com>
2021-10-01 19:41:45 -0300

    Implement Layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <TheHeader />
+
+        <div class="px-3 py-10 md:px-10">
+            <HelloWorld msg="Welcome to Your Vue.js App"/>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TheHeader from "./components/Partials/TheHeader.vue"
+    import HelloWorld from './components/HelloWorld.vue'
+    export default {
+        name: 'App',
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+        components: {
+          TheHeader,
+          HelloWorld
+        },
+
+        data() {
+            return {
+            };
+        },
+
+        methods: {},
+    };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
